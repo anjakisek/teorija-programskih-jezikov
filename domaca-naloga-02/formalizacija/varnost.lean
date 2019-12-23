586649f9f4d27cd7410accd7bdf496441d264da1
anja <anja@kisek.si>
2019-12-23 13:09:55 +0100

    zacetek dokazovanja

--- domaca-naloga-02/formalizacija/varnost.lean
+++ domaca-naloga-02/formalizacija/varnost.lean
@@ -342,7 +342,51 @@ begin
         cases Hof,
         apply Hof_a_2
     },
-    repeat {sorry},
+    case step.pair1 {
+        cases Hof,
+        apply of.pair,
+        apply Hstep_ih Hof_a,
+        apply Hof_a_1,
+    },
+    case step.pair2 {
+        cases Hof,
+        apply of.pair,
+        apply Hof_a,
+        apply Hstep_ih Hof_a_1,
+    },
+    case step.fst_step {
+        cases Hof,
+        apply of.fst,
+        apply Hstep_ih Hof_a,
+    },
+    case step.fst_beta {
+        cases Hof,
+        cases Hof_a,
+        apply Hof_a_a,
+    },
+      case step.snd_step {
+        cases Hof,
+        apply of.snd,
+        apply Hstep_ih Hof_a,
+    },
+    case step.snd_beta {
+        cases Hof,
+        cases Hof_a,
+        apply Hof_a_a_1,
+    },
+    case step.cons1 {
+        cases Hof,
+        apply of.cons,
+        apply Hstep_ih Hof_a,
+        apply Hof_a_1,
+    },
+    case step.cons2 {
+        cases Hof,
+        apply of.cons,
+        apply Hof_a,
+        apply Hstep_ih Hof_a_1,
+    },
+    repeat{sorry}
 end
 
 
